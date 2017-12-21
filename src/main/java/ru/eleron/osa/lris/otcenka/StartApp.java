5ce5150d704f70d4b8d124919ddd79cce0b03af7
Reyzor12 <testaccwow001@gmail.com>
2017-12-21 15:38:14 +0300

    base class start

--- src/main/java/ru/eleron/osa/lris/otcenka/StartApp.java
+++ src/main/java/ru/eleron/osa/lris/otcenka/StartApp.java
@@ -1,8 +1,33 @@
 package ru.eleron.osa.lris.otcenka;
 
-public class StartApp {
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-    public static void main(String[] args){
-       
+public class StartApp extends Application {
+
+    private static final Logger log = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("configuration/springContext.xml");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root = new AnchorPane();
+        primaryStage.setScene(new Scene(root,800,600));
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop(){
+
     }
 }
